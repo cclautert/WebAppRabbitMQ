80c914d6fe6127b9b722af7729d7bf584a371c0f
cristiano.c.lautert@gmail.com <cristiano.c.lautert@gmail.com>
2021-07-22 21:27:07 -0300

    Adicionar arquivos de projeto.

---
+++ AppRabbitMQ/Domain/Ordem.cs
@@ -0,0 +1,26 @@
+﻿namespace AppRabbitMQ.Domain
+{
+    public sealed class Ordem
+    {
+        private int _Numero;
+        public int Numero
+        {
+            get { return _Numero; }
+            set { _Numero = value; }
+        }
+
+        private string _Produto;
+        public string Produto
+        {
+            get { return _Produto; }
+            set { _Produto = value; }
+        }
+
+        private decimal _ValorTotal;
+        public decimal ValorTotal
+        {
+            get { return _ValorTotal; }
+            set { _ValorTotal = value; }
+        }
+    }
+}
---
+++ WebAppRabbitMQ/Controllers/OrdemController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebAppRabbitMQ.Domain;
+
+namespace WebAppRabbitMQ.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdemController : ControllerBase
+    {
+        private ILogger<Ordem> _logger;
+
+        public OrdemController(ILogger<Ordem> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult InsertOrdem(Ordem ordem)
+        {
+            try
+            {
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "OrdemQueue",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = JsonSerializer.Serialize(ordem);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "OrdemQueue",
+                                         basicProperties: null,
+                                         body: body);
+                }
+
+                return Accepted(ordem);
+            }catch (Exception ex)
+            {
+                _logger.LogError("Erro ao Inserir Ordem", ex);
+
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+}
